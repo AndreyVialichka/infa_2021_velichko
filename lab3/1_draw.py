152bf6d07b5fb214d04715a4debe978a8717b4ac
Velichko Andrey <forruexpress12@gmail.com>
2021-01-22 16:33:06 +0300

    classwork

--- lab3/1_draw.py
+++ lab3/1_draw.py
@@ -5,21 +5,45 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
+bg_color = (230, 230, 230)
+screen.fill(bg_color)
 
 N = 10
-color = (255, 255, 0)
-color1 = (255, 0, 0)
-color2 =(0,0,0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+black =(0, 0, 0)
+green = (9, 148, 65)
+brown = (101, 67, 33)
+blue = (0, 0, 176)
+yellow = (255,204,0)
+sky_blue = (63, 79, 232)
+tree_green = (144, 245, 0)
+# sky
+rect(screen, sky_blue, (0,0,400,200))
+# house
+rect(screen, green, (0,200,400,200))
+rect(screen, brown, (40,160,120,120))
+rect(screen, blue, (80,200,40,40))
+# grass
+polygon(screen, red, [(40,160),(100,80),(160,160)])
+# sun
+circle(screen, yellow, (340,60), 30)
+# tree
+line(screen, black, (280, 160), (280, 250), width = 15)
+circle(screen, tree_green, (260,140), 15)
+circle(screen, tree_green, (270,130), 15)
+circle(screen, tree_green, (280,150), 15)
+circle(screen, tree_green, (290,160), 15)
+circle(screen, tree_green, (300,120), 15)
+circle(screen, tree_green, (305,150), 15)
+# cloud
+circle(screen, white, (160,80), 20)
+circle(screen, white, (175,80), 20)
+circle(screen, white, (190,80), 20)
+circle(screen, white, (205,80), 20)
+circle(screen, white, (175,70), 30)
+circle(screen, white, (185,70), 30)
 
-circle(screen, color, (200,200), 100)
-circle(screen, color1, (150,180), 20)
-circle(screen, color1, (250,180), 20)
-circle(screen, color2, (150,180), 10)
-circle(screen, color2, (250,180), 10)
-
-line(screen, color2, (150, 250), (250, 250), width = 20)
-line(screen, color2, (280, 140), (230, 170), width = 15)
-line(screen, color2, (130, 150), (180, 180), width = 15)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
